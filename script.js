9a059b98e5b59e0717ccca1f1e353d8843025c4d
Nikhila <2420030387@klh.edu.in>
2026-04-17 23:03:36 +0530

    fixed API URL for deployment

--- script.js
+++ script.js
@@ -83,7 +83,7 @@ document.addEventListener('DOMContentLoaded', () => {
             submitBtn.disabled = true;
 
             try {
-                const response = await fetch('http://localhost:3000/api/login', {
+               const response = await fetch('https://campus-lost-found-8v49.onrender.com/api/login', {
                     method: 'POST',
                     headers: { 'Content-Type': 'application/json' },
                     body: JSON.stringify({ id, password, role })
